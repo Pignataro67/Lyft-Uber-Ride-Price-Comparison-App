6fb8a1df066974885ed55a7d2ec3b34e809e4bd9
Pignataro67 <Pignataro67@gmail.com>
2019-05-22 09:33:26 -0700

    adding files

--- lyft-uber-ride-price-comparison-app/client/src/App.js
+++ lyft-uber-ride-price-comparison-app/client/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import Card from './components/Card';
+// import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ConfirmRouteContainer from './containers/ConfirmRouteContainer';
+import ResultsContainer from './containers/ResultsContainer';
+import SearchContainer from './containers/SearchContainer';
+import { getMapboxKey } from './actions/fetchLocations'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.props.getMapboxKey()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>  
+          <Route exact path="/" component={SearchContainer}/>
+          <Route exact path="/confirm_route" component={ConfirmRouteContainer}/>
+          <Route exact path="/results" component={ResultsContainer}/>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {startingLocation: state.startingLocation, destination: state.destination}
+}
+
+const mapDispatchToProps = (dipatch) => {
+  return {getMapboxKey: () => dispatch(getMapboxKey())}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
