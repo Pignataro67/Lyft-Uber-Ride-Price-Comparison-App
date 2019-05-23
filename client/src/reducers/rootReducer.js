a21d0db2129ac439a5858d2a97aa5bd16eb6b12d
Pignataro67 <Pignataro67@gmail.com>
2019-05-23 09:22:58 -0700

    adding results folder and changes in conRou. comp

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,20 +1,30 @@
 export default function rootReducer(state = {
-    startingLocation: '', 
-    dropOff: '',
-    isFetchingStartingLocation: false,
-    isFetchingDropOff: false,
-    suggestedPickupLocations: [],
-    suggestedDropOff: [],
-    isConvertingPickupLatLong: false,
-    isConvertingDropoffLatLong: false,
-    pickupLatLong: '',
-    dropoffLatLong: '',
+  isFetchingMapboxKey: false,
+  mapBoxKey: '',
+  isFetchingStartingLocation: false,
+  isFetchingDropOff: false,
+  suggestedPickupLocations: [],
+  suggestedDropOff: [],
+  isConvertingPickupLatLong: false,
+  isConvertingDropoffLatLong: false,
+  pickupLat: '',
+  pickupLong: '',
+  dropoffLat: '',
+  dropoffLong: '',
+  isFetchingUberEstimate: false,
+  isFetchingLyftEstimate: false,
+  uberEstimates: '',
+  lyftEstimates: '',
    }, action) {
     switch(action.type){
-      case "FETCHING_SUGGESTED_START_LOCATIONS":
-        return {...state, isFetchingStartingLocation: true, suggestedPickupLocations: []}
+      case "FETCHING_MAPBOX_KEY":
+        return {...state, isFetchingMapboxKey: true}
+      case "ADD_MAPBOX_KEY_TO_STATE":
+        return {...state, isFetchingMapboxKey: false, mapBoxKey: action.key}
+      case "FETCHING_SUGGESTED_PICKUP_LOCATIONS":
+        return {...state, isFetchingPickupLocation: true, suggestedPickupLocations: []}
       case "DISPLAY_PICKUP_LOCATIONS":
-        return {...state, isFetchingStartingLocation: false, suggestedPickupLocations: action.suggestedPickupLocations}
+        return {...state, isFetchingPickupLocation: false, suggestedPickupLocations: action.suggestedPickupLocations}
       case "FETCHING_SUGGESTED_DROPOFFS": 
         return {...state, isFetchingDropOff: true, suggestedDropOffs: []}
       case "DISPLAY_DROPOFFS":
@@ -22,11 +32,19 @@ export default function rootReducer(state = {
       case "CONVERTING_PICKUP_LAT_LONG":
         return {...state, isConvertingPickupLatLong: true}
       case "RETRIEVE_PICKUP_LAT_LONG":
-        return{...state, isConvertingPickupLatLong: false, pickupLatLong: action.pickupLatLong}
+        return {...state, isConvertingPickupLatLong: false, pickupLatLong: action.pickupLatLong}
       case "CONVERTING_DROPOFF_LAT_LONG":
-        return{...state, isConvertingDropoffLatLong: true}
+        return {...state, isConvertingDropoffLatLong: true}
       case "RETRIEVE_DROPOFF_LAT_LONG":
-        return{...state, isConvertingDropoffLatLong: false, dropoffLatLong: action.dropoffLatLong}
+        return {...state, isConvertingDropoffLatLong: false, dropoffLatLong: action.dropoffLatLong}
+      case "RETURN_UBER_ESTIMATE":
+        return {...state, isFetchingUberEstimate: true, uberEstimates: ''}
+      case "ADD_UBER_ESTIMATE_TO_STATE":
+        return {...state, isFetchingUberEstimate: false, uberestimates: action.estimates }
+      case "FETCHING_LYFT_ESTIMATE":
+        return {...state, isFetchingLyftEstimate: true, lyftEstimates: ''}
+      case "ADD_LYFT_ESTIMATES_TO_STATE":
+        return {...state, isFetchingLyftEstimate: false, lyftEstimates: action.estimates }
         default: 
         return state
     }
