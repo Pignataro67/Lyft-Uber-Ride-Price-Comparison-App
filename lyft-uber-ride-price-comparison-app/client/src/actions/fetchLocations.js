392e9a2b776de9a2c67e6b71a6a87b53476fc63c
Pignataro67 <Pignataro67@gmail.com>
2019-05-24 11:01:38 -0700

    adding fetch estimate calls

--- lyft-uber-ride-price-comparison-app/client/src/actions/fetchLocations.js
+++ lyft-uber-ride-price-comparison-app/client/src/actions/fetchLocations.js
@@ -1,4 +1,4 @@
-function _fetchLocation(input) {
+function _fetchLocations(input) {
     return fetch(`RailsApi/search_locations/${input}`)
     .then(res => res.json())
     .then(locations => locations.predictions)
@@ -9,13 +9,27 @@ function _fetchLocation(input) {
     .then(res => res.json())
     .then(addressInfo => addressInfo.results[0].geometry.location)
   }
+
+  function _convertPickupLatLong(location) {
+    return (dispatch) => {
+      dispatch({type: 'CONVERTING_PICKUP_LAT_LONG'})
+      return _getLatLong(location).then(({ lat, long }) => dispatch({type: 'RETRIEVE_PICKUP_LAT_LONG', pickupLat: lat, pickupLong: long}))
+    }
+  }
+
+  function _convertDropoffLatLong(location) {
+    return (dispatch) => {
+      dispatch({type: 'CONVERTING_DROPOFF_LAT_LONG'})
+      return _getLatLong(location).then(({ lat, long }) => dispatch({type: 'RETRIEVE_DROPOFF_LAT_LONG', dropoffLat: lat, dropoffLong: long}))
+    }
+  }
   
-  export function fetchStartingLocation(input) {
+  export function fetchPickupLocation(input) {
     console.log(input)
       return (dispatch) => {
-        dispatch({ type: 'CONVERTING_START_LAT_LONG' })
-        return _getLatLong(location).then(({ lat, long}) => 
-      dispatch({ type: 'RETRIEVE_START_LAT_LONG', startlat: lat, startLong: long}))
+        dispatch({ type: 'FETCHING_SUGGESTED_PICKUP_LOCATIONS' });
+        _fetchLocations(input).then(suggestedPickupLocations => 
+      dispatch({ type: 'DISPLAY_PICKUP_LOCATIONS', suggestedPickupLocations }));
     };
   }
   
@@ -24,32 +38,33 @@ function _fetchLocation(input) {
       return (dispatch) => {
         dispatch({ type: 'FETCHING_SUGGESTED_DROPOFFS' });
         _fetchLocation(input).then(suggestedDropOffs => 
-      dispatch({ type: 'DISPLAY_DROPOFFS', suggestedDropoffs
-      }));
+      dispatch({ type: 'DISPLAY_DROPOFFS', suggestedDropoffs }));
     };
   }
   
-  export function convertPickupLatLong(location) {
-    console.log(location)
-    return (dispatch) => {
-      dispatch({type: 'CONVERTING_PICKUP_LAT_LONG'})
-      _getLatLong(location).then(startLatLong => dispatch({type: 'RETRIEVE_START_LAT_LONG', startLatLong}))
-    }
-  }
-  
-  export function convertDropoffLatLong(location) {
-    console.log(location)
-    return (dispatch) => {
-      dispatch({type: 'CONVERTING_DROPOFF_LAT_LONG'})
-      return _getLatLong(location).then(({ lat, long }) => dispatch({type: 'RETRIEVE_DROPOFF_LAT_LONG', dropoffLat: lat, dropOffLong: long }))
-    }
-  }
-  
   export function convertLatLong(pickupLocation, dropoffLocation) {
+    console.log(location)
     return async (dispatch) => {
-      await dispatch(convertPickupLatLong(pickupLocation))
-      await dispatch(convertDropoffLatLong(dropoffLocation))
+      await dispatch(_convertPickupLatLong(pickupLocation))
+      await dispatch(_convertDropoffLatLong(dropoffLocation))
     }
+  }
+  
+  export function fetchUberEstimate(pickupLat, pickupLong, dropoffLat, dropoffLong) {
+    return (dispatch) => {
+      fetch(`RailsApi/uber?pickupLat=${pickupLat}&pickupLong=${pickupLong}&dropoffLat=${dropoffLat}&dropoffLong=${dropoffLong}`)
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'ADD_UBER_ESTIMATES_TO_STATE', estimate: data.prices}))
+    };
+  }
+
+  export function fetchLyftEstimate(pickupLat, pickupLong, dropoffLat, dropoffLong) {
+    return (dispatch) => {
+      dispatch({ type: 'FETCHING_LYFT_ESTIMATE'});
+      fetch(`RailsApi/lyft?pickupLat=${pickupLat}&pickupLong=${pickupLong}&dropoffLat=${dropoffLat}&dropoffLong=${dropoffLong}`)
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'ADD_LYFT_ESTIMATES_TO_STATE', estimate: data.cost_estimates}))
+    };
   }
   
   export function getMapboxKey() {
@@ -60,3 +75,18 @@ function _fetchLocation(input) {
       .then(key => dispatch({ type: 'ADD_MAPBOX_KEY_TO_STATE', key }));
     };
   }
+
+  // export function convertDropoffLatLong(location) {
+  //   console.log(location)
+  //   return (dispatch) => {
+  //     dispatch({type: 'CONVERTING_DROPOFF_LAT_LONG'})
+  //     return _getLatLong(location).then(({ lat, long }) => dispatch({type: 'RETRIEVE_DROPOFF_LAT_LONG', dropoffLat: lat, dropOffLong: long }))
+  //   }
+  // }
+  
+  // export function convertLatLong(pickupLocation, dropoffLocation) {
+  //   return async (dispatch) => {
+  //     await dispatch(convertPickupLatLong(pickupLocation))
+  //     await dispatch(convertDropoffLatLong(dropoffLocation))
+  //   }
+  // }
